51b5110480d04c3408e91114332570baff6aa7af
IKala4I <vpersko@gmail.com>
2023-10-07 21:18:13 +0300

    + Created Navbar and footer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,8 +22,8 @@ export default function Hero() {
             <div className={'hero__image-container'}>
                 <div className={'hero__image'}>
                     <Image src={'/hero.png'} alt={'hero'} fill className={'object-contain'}></Image>
-                    <div className={'hero__image-overlay'}/>
                 </div>
+                <div className={'hero__image-overlay'}/>
             </div>
         </div>
     )
